a9c1981c4d08545c77c39ceadb7ee735cef6d63d
fsChronicles <123574767+fsChronicles@users.noreply.github.com>
2024-09-07 20:40:35 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.word-title').forEach(title => {
+        title.addEventListener('click', function() {
+            this.classList.toggle('active');
+            let content = this.nextElementSibling;
+            if (content.style.display === 'block') {
+                content.style.display = 'none';
+            } else {
+                content.style.display = 'block';
+            }
+        });
+    });
+});
